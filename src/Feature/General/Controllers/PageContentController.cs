81432a2c690f39767cbc4f847946aec6742eb6ac
rbatallasOshyn <rbatallas@oshyn.com>
2023-03-04 07:20:44 -0500

    imageCrop

---
+++ src/Feature/General/Controllers/PageContentController.cs
@@ -0,0 +1,29 @@
+﻿using CodeHat.Feature.GeneralContent.Sxa.Models;
+using CodeHat.Feature.GeneralContent.Sxa.Repositories;
+using Sitecore.XA.Foundation.Mvc.Controllers;
+using System.Web.Mvc;
+
+namespace CodeHat.Feature.GeneralContent.Sxa.Controllers
+{
+    public class PageContentController : StandardController
+    {
+        private readonly IImageCropRepository _imageCropRepository;
+
+        public PageContentController(IImageCropRepository imageCropRepository)
+        {
+            _imageCropRepository = imageCropRepository;
+        }
+
+        public ActionResult ImageCrop()
+        {
+            var model = (ImageCropModel)_imageCropRepository.GetModel();
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public ActionResult UploadAvatar(string data)
+        {
+            return Json(new { data });
+        }
+    }
+}
---
+++ src/Feature/General/Repositories/IImageCropRepository.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.XA.Foundation.Mvc.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHat.Feature.GeneralContent.Sxa.Repositories
+{
+    public interface IImageCropRepository: IModelRepository
+    {
+        void SaveImageCrop(string data, string dataSourceId);
+    }
+}
