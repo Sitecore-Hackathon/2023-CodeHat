c9b5ac5b4f5bea0f8f7016f2384de41a138bf2cb
Mariela <Mariela@DESKTOP-MAHITDI>
2023-03-04 08:24:19 -0500

    Unicorn configuration. Expose api service

---
+++ src/Feature/General/Pipelines/RegisterWebApiRoutes.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CodeHat.Feature.GeneralContent.Sxa.Pipelines
+{
+    public class RegisterWebApiRoutes
+    {
+#pragma warning disable RECS0154 // Parameter is never used
+        public void Process(PipelineArgs args)
+#pragma warning restore RECS0154 // Parameter is never used
+        {
+            RouteTable.Routes.MapRoute("uploadimage", "api/{controller}/{action}",
+                new { controller = "ImageSxa", action = "UploadImage" });            
+
+        }
+    }
+}
