299deab507b40ba6145482daaf84c684a15b8ec6
rbatallasVertic <rbatallas@vertic.com>
2023-03-04 09:40:35 -0500

    add page content project

---
+++ src/Feature/PageContent/code/Controllers/PageContentController.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.XA.Foundation.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeHat.Feature.PageContent.Controllers
+{
+    public class PageContentController: StandardController
+    {
+        public ActionResult ImageCrop() 
+        { 
+            return View(); 
+        }
+    }
+}
---
+++ src/Feature/PageContent/code/Models/ImageCropModel.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.XA.Foundation.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeHat.Feature.PageContent.Models
+{
+    public class ImageCropModel: RenderingModelBase
+    {
+
+    }
+}
